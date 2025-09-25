05abeb7742e23ff04a552aed123faad15cf0caf1
Kev1nCanCode <kevinjungmann@hotmail.com>
2025-09-25 18:57:39 +0200

    Added system arguments

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import sys
 from stats import count_words, count_chars,print_report, sorted_dict, sort_dict_helper
+#("books/frankenstein.txt"
 
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+    
 def main():
-    print_report("books/frankenstein.txt")
+    print_report(f"{sys.argv[1]}")
+
 
 main()
